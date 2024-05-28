dcbc942df62bd050d82715d9b27cb80c64cdb478
Nishant <nishantchaudhary842@gmail.com>
2024-05-28 13:01:39 +0545

    refactor and defensive

---
+++ session-6-defensive_coding/task1.py
@@ -0,0 +1,37 @@
+# Task 1: Refactoring
+# Objective: Refactor the provided code snippet to improve readability and maintainability.
+
+# Instructions:
+# Identify areas of the code that can be improved.
+# Apply refactoring techniques such as renaming variables, extracting functions, simplifying conditional expressions, and removing code duplication.
+# Ensure the refactored code produces the same output as the original code.
+
+
+# Original Code Snippet:
+def process_transactions(transactions):
+    for t in transactions:
+            if t['type'] == 'credit':
+                if t['amount'] > 0:
+                    print(f"Credit transaction of ${t['amount']}")
+                else:
+                    print("Invalid credit amount")
+            elif t['type'] == 'debit':
+                if t['amount'] > 0:
+                    print(f"Debit transaction of ${t['amount']}")
+                else:
+                    print("Invalid debit amount")
+            else:
+                print("Unknown transaction type")
+
+transactions = [
+        {'type': 'credit', 'amount': 100},
+        {'type': 'debit', 'amount': 50},
+        {'type': 'credit', 'amount': -20},
+        {'type': 'transfer', 'amount': 30}
+    ]
+
+process_transactions(transactions)
+
+
+
+# will do soon
---
+++ session-6-defensive_coding/task2.js
@@ -0,0 +1,35 @@
+const processCalculation = (tax, prices) => {
+  const checkValidation = {
+    checkPrice: (price) => {
+      if (typeof price != "number") {
+        throw new Error("Price should be number");
+      } else {
+        return price;
+      }
+    },
+    checkTax: (tax) => {
+      if (typeof tax != "number") {
+        throw new Error("Tax should be number");
+      } else {
+        return tax;
+      }
+    },
+  };
+
+  try {
+    let total_amount = 0;
+
+    prices.forEach((item) => {
+      const validate = checkValidation.checkPrice(item);
+      const checkTax = checkValidation.checkTax(tax);
+
+      total_amount = item + item * checkTax;
+    });
+
+    return total_amount;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+processCalculation(0.4, [12, 24]);
